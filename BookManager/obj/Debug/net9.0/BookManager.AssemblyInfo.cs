f0726509c66f4378af8738060d5184acf9d994ba
Mukesh Murugan <iammukeshm@gmail.com>
2024-11-23 17:12:19 +0530

    ci: add build yml

--- BookManager/obj/Debug/net9.0/BookManager.AssemblyInfo.cs
+++ BookManager/obj/Debug/net9.0/BookManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73ef857a5ccd152c7725ac7476921bef3ad2c9db")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
